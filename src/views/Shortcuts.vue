950219caddb6bf2c1f33088e53e3e7f7c0bc9c2d
GeordieTomo <Geordietomomcclelland@gmail.com>
2022-01-18 13:42:17 +1100

    update shortcuts page
    made window more dynamically resizable, tiles will stretch to fill the window

--- src/views/Shortcuts.vue
+++ src/views/Shortcuts.vue
@@ -3,82 +3,85 @@
             
             <router-link to="/">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile">
-                    <div class="uk-grid-column-small uk-padding-remove uk-margin-remove" uk-grid>
-                        <i class="fa fa-home center-icon"></i>
-                        <span class="uk-text uk-width-2-3 uk-padding-remove">Home</span>
-                    </div>
+                    <i class="fa fa-home center-icon"></i>
+                    <span class="tile-text">Home</span>
                 </button>
             </router-link>
 
             <router-link to="/settings">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile">
-                    <div class="uk-grid-column-small uk-padding-remove uk-margin-remove" uk-grid>
                         <i class="fa fa-gear center-icon"></i>
-                        <span class="uk-text uk-width-2-3 uk-padding-remove">Settings</span>
-                    </div>
+                        <span class="tile-text">Settings</span>
                 </button>
             </router-link>
             
             <router-link to="/profile">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile">
-                    <div class="uk-grid-column-small uk-padding-remove uk-margin-remove" uk-grid>
                         <i class="fa fa-line-chart center-icon"></i>
-                        <span class="uk-text uk-width-2-3 uk-padding-remove">Adjust Profile</span>
-                    </div>
+                        <span class="tile-text">Adjust Profile</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
-                </button>
-            </router-link>
-            
-            <router-link to="/shortcuts">
-                <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
             <router-link to="/shortcuts">
                 <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
-                    Free Button
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
+                </button>
+            </router-link>
+
+            <router-link to="/shortcuts">
+                <button class="uk-button uk-button-large uk-button-default uk-width-1-1 button-tile" disabled>
+                        <i class="fa fa-times center-icon"></i>
+                        <span class="tile-text">Free Button</span>
                 </button>
             </router-link>
 
@@ -96,16 +99,26 @@ name: "Shortcuts",
 
 <style>
 .button-tile {
-    font-size: 1.2em; 
-    height:130px;
+    font-size: min(calc(5vh - 9px), 14px);
+    height: calc(33vh - 54px);
     padding: 0%;
     border-radius: 8px;
 }
 
 .center-icon {
-    font-size: 1.2em;
+    font-size: 4em;
+    padding: calc(0);
     display: flex;
     align-items:center;
+    justify-content: center;
+}
+
+.tile-text {
+    font-size: 1em;
+    padding-top: 2vh;
+    line-height: 120%;
+    display: flex;
+    justify-content: center;
 }
 
 </style>

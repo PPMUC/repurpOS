af80ea7843b02d41b16a97a4b2784c1cfacfd131
Hannah <htay0008@student.monash.edu>
2022-04-30 21:53:20 +1000

    manual overrides

--- src/components/StatusNumbers.vue
+++ src/components/StatusNumbers.vue
@@ -1,51 +1,120 @@
 <template>
-      <div class="uk-section uk-section-muted uk-padding-remove uk-margin-remove" uk-height-viewport="offset-top: true">
-        <div class="uk-flex uk-padding-small uk-margin-remove-left uk-margin-remove-horizontal uk-margin-top" uk-grid>
-          <div class="uk-flex uk-margin-remove uk-padding-remove uk-width-1-1">
-            <NumDisplay class="uk-width-1-2" v-for="tcon in temperatureControllers" :key="tcon.name"  :num="tcon.current" 
-            :unit="'&deg;C'" :name="tcon.name" :isBig="true" :setpoint="tcon.setpoint">
-            </NumDisplay>
+  <div
+    class="uk-section uk-section-muted uk-padding-remove uk-margin-remove"
+    uk-height-viewport="offset-top: true"
+  >
+    <div
+      class="
+        uk-flex
+        uk-padding-small
+        uk-margin-remove-left
+        uk-margin-remove-horizontal
+        uk-margin-top
+      "
+      uk-grid
+    >
+      <!-- TOP AND BOTTOM PLATE TEMPERATURES -->
+      <div class="uk-flex uk-margin-remove uk-padding-remove uk-width-1-1">
+        <NumDisplay
+          class="uk-width-1-2"
+          v-for="tcon in temperatureControllers"
+          :key="tcon.name"
+          :num="tcon.current"
+          :unit="'&deg;C'"
+          :name="tcon.name"
+          :isBig="true"
+          :setpoint="tcon.setpoint"
+        >
+        </NumDisplay>
+      </div>
+      <!-- INPUTS FOR MANUAL OVERRIDES -->
+      <div class="uk-flex uk-padding-remove uk-width-1-1" uk-grid>
+        <div
+          v-for="tcon in temperatureControllers"
+          :key="tcon.name"
+          class="uk-width-1-2"
+        >
+          <div>
+            <input type="number" v-model="value" :placeholder="tcon.setpoint" />
           </div>
-          <div class="uk-flex uk-padding-remove uk-width-1-1" uk-grid>
-            <div v-for="num in smallSensors" :key="num.name" class="uk-width-1-3">
-              <NumDisplay :key="num.name" :num="num.current" :unit="OPTIONAL_SENSOR_INFO[num.name].units" :name="num.name">
-              </NumDisplay>
-            </div>
-          </div>
-
-
+          <button
+            @click="override(tcon.name, value)"
+            class="uk-button uk-button-small uk-width-1-1 button-padding"
+          >
+            <span class="uk-text">Override</span>
+          </button>
         </div>
       </div>
+      <!-- HOOD AND FRAME TEMPERATURES -->
+      <div class="uk-flex uk-padding-remove uk-width-1-1" uk-grid>
+        <div v-for="num in smallSensors" :key="num.name" class="uk-width-1-2">
+          <NumDisplay
+            :key="num.name"
+            :num="num.current"
+            :unit="OPTIONAL_SENSOR_INFO[num.name].units"
+            :name="num.name"
+          >
+          </NumDisplay>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  // @ is an alias to /src
-  import NumDisplay from "@/components/NumDisplay"
-  import * as machineVariables from "@/controller/machine_info";
-  import { mapGetters } from 'vuex'
+// @ is an alias to /src
+import NumDisplay from "@/components/NumDisplay";
+import * as machineVariables from "@/controller/machine_info";
+import { mapGetters } from "vuex";
 
-  export default {
-    name: "StatusNumbers",
-    components: {
-      NumDisplay
+export default {
+  name: "StatusNumbers",
+  components: {
+    NumDisplay,
+  },
+  data: function () {
+    return {
+      OPTIONAL_SENSOR_INFO: machineVariables.OPTIONAL_SENSOR_INFO,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      optionalSensors: "machine/optionalSensors",
+      temperatureControllers: "machine/temperatureControllers",
+    }),
+    smallSensors() {
+      return this.optionalSensors;
     },
-    data: function () {
-      return {
-        OPTIONAL_SENSOR_INFO: machineVariables.OPTIONAL_SENSOR_INFO
-      };
-    },
-    computed:{
-      ...mapGetters({
-        optionalSensors: 'machine/optionalSensors',
-        temperatureControllers: 'machine/temperatureControllers',
-      }),
-      smallSensors(){
-        return this.optionalSensors;
+  },
+  methods: {
+    override(name, value) {
+      console.log(`override setpoint of ${name}`);
+      // if input is within limits
+      if (
+        value > machineVariables.HEATING_ZONES[0].limits[0] &&
+        value < machineVariables.HEATING_ZONES[0].limits[1]
+      ) {
+        // update setpoint accordingly
+        switch (name) {
+          case "top plate":
+            this.$store.state.machine.tempControllers[0].setpoint = value;
+            return;
+          case "bottom plate":
+            this.$store.state.machine.tempControllers[1].setpoint = value;
+            return;
+        }
+      } else {
+        // display warning
+
+        // reset input field
+        this.value = "";
+        return;
       }
-    }
-  };
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-  @use "../assets/css/variables";
+@use "../assets/css/variables";
 </style>
